ea69203ff03ba0587b5e50a06253f63e04f68e32
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-12-03 13:15:29 +0700

    feat[minitask 4]: add Home Landing Page And feature review In Home.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <footer className="p-10 mt-20 h-[15vh] bg-[#6DC3BB] font-bold ">
+            <div>Ini Footer</div>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
+import Footer from '../components/Footer.jsx'
 import Heading from '../components/Heading.jsx'
 import Review from '../components/Review.jsx'
 
 function Home() {
   return (
-     <>
+    <>
       <Heading title={"Bian Albaihaqi"} />
       <div>
         <h2 className='text-4xl text-center font-bold mt-7'>Review</h2>
         <Review/>
       </div>
+      <Footer/>
     </>
   )
 }
