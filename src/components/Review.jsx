96e94b6ec244406eba561f7db4a70e0bef12bb56
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-12-03 13:11:52 +0700

    feat[minitask 4]: add Home Landing Page And feature review In Home.jsx

---
+++ src/components/Review.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Review() {
+  return (
+    <div className='flex justify-center mt-5'>
+        <form className='flex flex-col w-[500px] gap-5'>
+            <input type="text" placeholder="Name" className="border p-2 rounded-md" />
+            <input type="Email" placeholder="Email" className="border p-2 rounded-md" />
+            <textarea name="message" className="border p-2 rounded-md h-[200px] resize-none" ></textarea>
+
+            <button type="submit" className= "bg-blue-500 text-white py-2 rounded-md cursor-pointer hover:bg-blue-400">
+                Tambah
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default Review
